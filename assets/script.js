f616506f613b215a35274a1be742cab7131b888b
BunuLuDunu <laurine.sassano@gmail.com>
2024-03-21 11:46:26 +0100

    Added dot for each slide

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,32 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+// On récupère les flèches du slider
+const leftArrow = document.querySelector(".arrow_left");
+const rightArrow = document.querySelector(".arrow_right");
+
+// On récupère la div des points du slider
+const dots = document.querySelector(".dots");
+
+// Slide séléctionnée
+let currentSlide = 0;
+
+// Ajout des points par élément dans le carrousel
+for (let i = 0; i < slides.length; i++) {
+	let dot = document.createElement("button");
+	dot.classList.add(".dot");
+	dots.appendChild(dot);
+}
+
+// Ajout d'un event listener sur les flèches du carrousel
+leftArrow.addEventListener("click", (event) => {
+	goTo(currentSlide - 1)
+	console.log("prev");
+})
+
+rightArrow.addEventListener("click", (event) => {
+	goTo(currentSlide + 1)
+	console.log("next");
+})
