2babb3f0831af36202748832181e259fb79660e9
BunuLuDunu <laurine.sassano@gmail.com>
2024-03-21 13:11:48 +0100

    Added function to change slide

--- assets/script.js
+++ assets/script.js
@@ -19,29 +19,61 @@ const slides = [
 
 
 // On récupère les flèches du slider
-const leftArrow = document.querySelector(".arrow_left");
-const rightArrow = document.querySelector(".arrow_right");
+const prevBtn = document.querySelector(".arrow_left");
+const nextBtn = document.querySelector(".arrow_right");
 
 // On récupère la div des points du slider
 const dots = document.querySelector(".dots");
+
+// On récupère l'image du slider
+const bannerImg = document.querySelector(".banner-img");
+
+// On récupère la tagline
+const bannerTagline = document.querySelector("#banner p");
 
 // Slide séléctionnée
 let currentSlide = 0;
 
 // Ajout des points par élément dans le carrousel
 for (let i = 0; i < slides.length; i++) {
-	let dot = document.createElement("button");
-	dot.classList.add(".dot");
-	dots.appendChild(dot);
+	let dot = document.createElement("span"); // On créé un bouton 
+	dot.classList.add("dot"); 	// On lui ajoute la classe .dot
+
+	// Ajouter la classe dot_selected au premier point
+	if(i === currentSlide) {
+		dot.classList.add('dot_selected') 
+	}
+
+	dots.appendChild(dot); 	// On l'ajoute à la div parente .dots
 }
 
 // Ajout d'un event listener sur les flèches du carrousel
-leftArrow.addEventListener("click", (event) => {
-	goTo(currentSlide - 1)
-	console.log("prev");
+prevBtn.addEventListener("click", () => {
+	goTo(currentSlide - 1);
 })
 
-rightArrow.addEventListener("click", (event) => {
-	goTo(currentSlide + 1)
-	console.log("next");
+nextBtn.addEventListener("click", () => {
+	goTo(currentSlide + 1);
 })
+
+// Fonction pour changer d'image
+function goTo(index) {
+	if(index < 0) {
+		currentSlide = slides.length - 1;
+	} else if(index > slides.length - 1) {
+		currentSlide = 0;
+	} else {
+		currentSlide = index
+	}
+
+	// Parcours les enfants de la div dots pour ajouter/supprimer la classe dot_selected
+	for(let i = 0; i < dots.children.length; i++) {
+		dots.children[i].classList.toggle('dot_selected', i === currentSlide)
+	}
+
+	// On change la source de l'image en fonction de l'index
+	bannerImg.src = "./assets/images/slideshow/" + slides[currentSlide].image;
+
+	// On change la tagline en fonction de l'index
+	bannerTagline.innerHTML = slides[currentSlide].tagLine;
+}
